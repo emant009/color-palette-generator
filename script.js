d47b9079fb479bc3c9d43cd2a07694f5ba1627ed
Eman Tahir <ett429576@gmail.com>
2026-07-13 23:51:46 +0500

    feat: make it responsive

--- script.js
+++ script.js
@@ -5,5 +5,9 @@ generateButton.addEventListener("click", generatePalette);
 
 function generatePalette() {
   const colors = []
-  
+  for (let i = 0; 
+     < array.length; index++) {
+    const element = array[index];
+    
+  }
 }
